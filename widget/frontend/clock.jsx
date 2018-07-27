414f109d1d601ebaea211c1d00b836325bddd749
JordanYu4 <jordanyu44@gmail.com>
2018-07-27 14:39:24 -0700

    Set up react in widgets project

---
+++ widget/frontend/clock.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1>Clock</h1>
+    );}
+}
+
+export default Clock;
--- widget/webpack.config.js
+++ widget/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/entry.jsx',
+  entry: './frontend/widgets.jsx',
   output: {
     filename: './bundle.js',
     path: path.resolve(__dirname)
