93776901083e27209c97127a6765a5eea66649e4
JordanYu4 <jordanyu44@gmail.com>
2018-07-27 17:06:41 -0700

    Build working clock widget

--- widget/frontend/clock.jsx
+++ widget/frontend/clock.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
 class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      initial: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.tickInterval = setInterval(this.tick.bind(this), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.tickInterval);
+  }
+
+  tick() {
+    this.setState({ initial: new Date() });
+  }
+
   render() {
     return (
-      <h1>Clock</h1>
-    );}
+      <div>
+        <h1>Clock</h1>
+        <div className="clock-container">
+          <label>
+            Time:
+            <span> </span>
+            {`${this.state.initial.getHours()}`}:
+            {`${this.state.initial.getMinutes()}`}:
+            {`${this.state.initial.getSeconds()}`}
+            <span> </span>
+            PDT
+          </label>
+          <br />
+          <label>
+            Date:
+            <span> </span>
+            {this.state.initial.toUTCString().slice(0, 16)}
+          </label>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default Clock;
